42f776b7666afc6715f0b9d2b3fd9d86a69afec9
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-01 20:51:50 +0300

    Task 1

---
+++ Seminar_1/SeminarTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine ("Введите первое число: ");
+int a = int.Parse(Console.ReadLine ());
+
+Console.WriteLine ("Введите второе число: ");
+int b = int.Parse(Console.ReadLine ());
+
+if (a * a == b){
+    Console.WriteLine("Yes");
+}
+else{
+    Console.WriteLine("No");
+}
