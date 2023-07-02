3ccb814d56b8abdfa7d15ff5ddc43aa5aaa7022e
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-02 20:05:23 +0300

    Удаляет 2-ую цифру из трехзначного числа

---
+++ Seminar_2/SeminarTask2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе удаляет вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine(randomNumber);
+
+int result = ((randomNumber/100)*10)+(randomNumber % 10);
+Console.WriteLine(result);
+
