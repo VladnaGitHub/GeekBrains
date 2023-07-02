5f33c01e5f392b2cd181d98f5961ff67cb2a70e2
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-02 20:49:09 +0300

    Проверка на квадрат

---
+++ Seminar_2/SeminarTask5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите прогармму, которая на вход принимает два числа и проверяет, является ли одно число квадратом другого.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if ((a * a == b) || (b * b == a))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
