dcbfff6a8101bf92e999683828f2ac6e2fd0007e
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-02 20:21:07 +0300

    Кратно, не кратно

---
+++ Seminar_2/SeminarTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {a % b}");
+}
