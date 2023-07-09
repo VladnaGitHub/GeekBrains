4e17c7a56c4ba31e14328d4039d359ab9b41617f
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-09 23:19:54 +0300

    Таблица квадратов чисел от 1 до N

---
+++ Seminar_3/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+
+void PrintSgrTab(int N)
+{
+    if (N < 1)
+    {
+        Console.WriteLine("There are no valid numbers");
+    }
+    else
+    {
+        Console.Write($"{N} -> ");
+        for (int i = 1; i <= N; i++)
+        {
+            Console.Write($"{Math.Pow(i, 2)} ");
+        }
+    }
+}
+
+// -----------
+
+Console.Write("Please, n: ");
+int N = int.Parse(Console.ReadLine());
+PrintSgrTab(N);
