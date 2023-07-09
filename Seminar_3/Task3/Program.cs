2641053b30be5f798fd738d897f34408a5ded851
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-09 21:23:54 +0300

    Switch_Case

---
+++ Seminar_3/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Методы (Функции, процедуры)
+ [Возвращаемый тип] [Имя метода - глагол]([Параметры метода])
+ {
+    Код метода
+}*/
+
+void PrintIntervals(int n)
+{
+        switch (n)
+        {
+            case 1:
+                Console.WriteLine("X > 0, y > 0");
+                break;
+            case 2:
+                Console.WriteLine("X < 0, y > 0");
+                break;
+            case 3:
+                Console.WriteLine("X < 0, y < 0");
+                break;
+            case 4:
+                Console.WriteLine("X > 0, y < 0");
+                break;
+                default:
+                Console.WriteLine("Error");
+                break;
+        }
+    }
+
+// -----------------------------------------
+
+Console.WriteLine("Please, enter integer number of quater: ");
+int Num = int.Parse(Console.ReadLine());
+
+PrintIntervals(Num);
