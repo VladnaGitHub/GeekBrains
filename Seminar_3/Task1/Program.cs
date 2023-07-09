8e8fc07c4a0555744b4d21d59525eacaa9b1ef4f
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-09 23:30:07 +0300

    Вывод третьей цифры из числа

---
+++ Seminar_3/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 13 по 2-му семинару. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое целое число: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+while (userNumber >= 1000)
+{
+    userNumber = userNumber / 10;
+}
+int thirdDigit = userNumber % 10;
+Console.WriteLine($"Третья цифра числа {thirdDigit}");
