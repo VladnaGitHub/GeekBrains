677de523b1dde8463cff5323fe1a92f601d871da
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-09 19:45:13 +0300

    Строки

---
+++ Seminar_3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//
+
+Console.WriteLine("Введите любое целое число: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+string userNumberStr = Math.Abs(userNumber).ToString();
+if (userNumberStr.Length < 3)
+{
+    Console.WriteLine("В вашем числе нет третьей цифры");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа: {userNumberStr[2]}");
+}
