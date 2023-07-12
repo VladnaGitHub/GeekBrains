20fde451725cf711ae09f7ec5a49bf5fd25471b2
Vladislav <kozel.vladislaw2022@yandex.ru>
2023-07-13 02:08:59 +0300

    Homework_Task1

---
+++ Seminar_3/Homework_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Please put 5 digits number: ");
+int aByUser = int.Parse(Console.ReadLine());
+string polindrom = Convert.ToString(aByUser);
+
+if (polindrom.Length != 5)
+{
+    Console.WriteLine("You number is NOT 5 digits number");
+}
+else if (polindrom [0] == polindrom[4] && polindrom[1] == polindrom[3])
+{
+    Console.WriteLine($"You number: {polindrom} is the polindrom");
+}
+else
+{
+    Console.WriteLine("The number you put is not a polindrom");
+}
